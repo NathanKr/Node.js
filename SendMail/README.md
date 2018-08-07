c30ea0b42d0f87276344c3ab5c088db055cf3a16
Nathan Krasney <natankrasney@gmail.com>
2018-08-07 13:43:04 +0300

    small change

--- SendMail/README.md
+++ SendMail/README.md
@@ -2,7 +2,7 @@
 
 
 <h3>Points of interest</h3>
-<li>The sample is working but for some reasons mail are sent to the email spam directory</li>
+<li>The sample is working but  some mails are received in the email spam directory, This may be improved if the mail start \ end with correct names</li>
 
 
 <h3>Installation</h3>
--- SendMail/send_email.js
+++ SendMail/send_email.js
@@ -1,8 +1,8 @@
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
-  to: 'natankrasney@gmail.com',
-  from: 'natankrasney@gmail.com',
+  from : 'natankrasney@gmail.com',
+  to : 'theyallabeitar1@gmail.com',
   subject: 'Meeting ',
   text: 'Hi Nathan  easy to do anywhere, even with Node.js Nathan'
 };
