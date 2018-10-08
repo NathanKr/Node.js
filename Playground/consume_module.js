c8e30eea134dcf303d1989b4373b50f256b85002
Nathan Krasney Laptop <natankrasney@gmail.com>
2018-10-08 14:51:03 +0300

    custome module

--- Playground/consume_module.js
+++ Playground/consume_module.js
@@ -1,9 +1,12 @@
 var http = require('http');
+var mod1 = require('./module1');
 console.log('open the browser at localhost:\8080')
 
 const listener = (request, response) => {
     response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('<h1 style="color:red;">Hello Node.js!</h1>');
+    response.write(`max (3,4) : ${mod1.max(3,4)}`);
+    response.write("<br/>");
+    response.write(`text is : ${mod1.t1}`);
     response.end();
 } 
 
---
+++ Playground/module1.js
@@ -0,0 +1,11 @@
+function max(a, b) {
+  return a > b ? a : b;
+}
+
+// --- export the function max as max
+exports.max = max;
+
+const text = 'hello world';
+
+// --- export the variable text as t1
+exports.t1 = text;
