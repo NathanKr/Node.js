b8c41d093e4240ca1e940ba2978e5f276d80d339
Nathan Krasney Laptop <natankrasney@gmail.com>
2018-10-11 12:51:32 +0300

    read users from collection

--- MongoDB/simple_server/read_users.js
+++ MongoDB/simple_server/read_users.js
@@ -4,25 +4,17 @@ const constants = require("./constants");
 
 const mongoClient = mongo.MongoClient;
 
-const insertUserHandler = (err, res) => {
-  if (err) throw err;
-
-  console.log("insert is success");
-};
 
 const connectHandler = (err, db) => {
   if (err) throw err;
 
   const dbObject = db.db(dev.dbName);
   const userCollection = dbObject.collection(constants.usersCollection);
-  userCollection.insertOne(
-    { id: 1, name: "Nathan", age: 55 },
-    insertUserHandler
-  );
-  userCollection.insertOne(
-    { id: 2, name: "Nitzan", age: 27 },
-    insertUserHandler
-  );
+  const cursor = userCollection.find({});// --- same as select *
+  cursor.forEach(elem => {
+    console.log(elem)
+  });
+
   db.close();
 };
 
