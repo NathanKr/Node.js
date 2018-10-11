62c7ebc47f0cf1e8cdf6ea1dc734fce0f630149d
Nathan Krasney Laptop <natankrasney@gmail.com>
2018-10-11 15:46:51 +0300

    some change

--- MongoDB/simple_server/delete_users.js
+++ MongoDB/simple_server/delete_users.js
@@ -4,16 +4,19 @@ const constants = require("./constants");
 
 const mongoClient = mongo.MongoClient;
 
+const deleteHandler =  (err, result) => {
+  if (err) throw err;
+  console.log(`${result.deletedCount} document was deleted`);
+}
+
 const connectHandler = (err, db) => {
   if (err) throw err;
 
   const dbObject = db.db(dev.dbName);
   const userCollection = dbObject.collection(constants.usersCollection);
-  const query = { id: 1 };
-  userCollection.deleteOne(query, (err, result) => {
-    if (err) throw err;
-    console.log("one document was deleted");
-  });
+  
+  userCollection.deleteOne({ id: 1 },deleteHandler);
+  userCollection.deleteOne({ id: 2 },deleteHandler);
 
   db.close();
 };
