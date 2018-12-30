299bda2761b1313a6c9f3f4c30c3d67dc15287a3
Nathan Krasney <natankrasney@gmail.com>
2018-12-30 17:45:42 +0200

    simple sync promise

---
+++ NextGenerationJS/Promise/simple.js
@@ -0,0 +1,17 @@
+/*
+    promise are used for async operation
+    here the operation is sync but it is easier to understand
+*/
+
+const promise_success = new Promise((resolve, reject) => {
+  resolve("this is resolve");
+});
+
+promise_success.then(success => console.log("inside then : ", success));
+
+const promise_error = new Promise((resolve, reject) => {
+    reject("this is reject");
+  });
+  
+promise_error.catch(error => console.log("inside catch : ", error));
+  
