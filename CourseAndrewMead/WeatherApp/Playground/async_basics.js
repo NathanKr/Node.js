f285bcd112b7a4d5591c2ab07ac73cfffbe661b1
Nathan Krasney Laptop <natankrasney@gmail.com>
2018-12-30 09:18:17 +0200

    very initial

---
+++ CourseAndrewMead/WeatherApp/Playground/async_basics.js
@@ -0,0 +1,7 @@
+console.log("app started");
+
+setTimeout(() => console.log("inside timer handler of setTimeout 3000"), 3000);
+
+setTimeout(() => console.log("inside timer handler of setTimeout 0"), 0);
+
+while (true) {}
---
+++ CourseAndrewMead/WeatherApp/app.js
@@ -0,0 +1,5 @@
+console.log("app is loading");
+
+const dev = require("./app_dev");
+
+console.log(dev.API_KEY)
