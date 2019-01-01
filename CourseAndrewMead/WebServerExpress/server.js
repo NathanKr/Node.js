704c3d4094604c7d6bc0d6c2a9584724646e4cef
Nathan Krasney Laptop <natankrasney@gmail.com>
2019-01-01 15:46:57 +0200

    minor change

--- CourseAndrewMead/WebServerExpress/server.js
+++ CourseAndrewMead/WebServerExpress/server.js
@@ -11,7 +11,7 @@ app.get("/about", (req, res) => {
   res.send("<h1>This is about</h1>");
 });
 
-// --- mount middleware to serve static files e.g. html\image inside public file
+// --- mount middleware to serve static files e.g. html\image inside public directory
 app.use(express.static(__dirname + "/public"));
 
 // --- listen for requests on this port
