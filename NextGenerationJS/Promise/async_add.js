91c00f1c1e9dbb6410ff50e3df50dd4fc1007d9b
Nathan Krasney Laptop <natankrasney@gmail.com>
2019-01-01 10:54:13 +0200

    small output

--- NextGenerationJS/Promise/async_add.js
+++ NextGenerationJS/Promise/async_add.js
@@ -1,17 +1,17 @@
 const async_add = (a, b) => {
   return new Promise((resolve, reject) => {
+    console.log('inside promise')
     setTimeout(() => {
       if (typeof a === "number" && typeof b === "number") {
         resolve(a + b);
       } else {
         reject("type error");
       }
-    }, 2000);
+    }, 3000);
   });
 };
 
-a1 = async_add(2,3);
-a2 = async_add("a",3);
+a1 = async_add(2, 3);
+a2 = async_add("a", 3);
 a1.then(result => console.log(result)).catch(err => console.log(err));
 a2.then(result => console.log(result)).catch(err => console.log(err));
-
