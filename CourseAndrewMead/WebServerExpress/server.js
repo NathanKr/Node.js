a28e209e6db189ea20bcd2dc5ef1051ebe5a9c94
Nathan Krasney Laptop <natankrasney@gmail.com>
2019-01-01 17:45:29 +0200

    add express middleware

--- CourseAndrewMead/WebServerExpress/server.js
+++ CourseAndrewMead/WebServerExpress/server.js
@@ -1,12 +1,37 @@
+console.log("app is loading");
+const fs = require("fs");
 const express = require("express");
+const fileName = "server.log";
 
 // --- create an app
 const app = express();
 
+/* 
+        next() mark that this middleware has finished
+
+        it is important to be used before the request handlers
+*/
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const info = `time : ${now} ,method : ${req.method} , url : ${req.url}`;
+  console.log(info);
+  try {
+    fs.appendFileSync(fileName, `${info}\n`);
+  } catch (err) {
+    console.log(`Error appendFileSync : ${err.message}`);
+  }
+
+  console.log(info);
+
+  next();
+});
+
+// --- handle request to /
 app.get("/", (req, res) => {
   res.send({ hello: "express" });
 });
 
+// --- handle request to /about
 app.get("/about", (req, res) => {
   res.send("<h1>This is about</h1>");
 });
