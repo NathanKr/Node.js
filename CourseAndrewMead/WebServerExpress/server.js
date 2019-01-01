ba41e4fa2187f4bd390f820edc3f0ac6c6005788
Nathan Krasney Laptop <natankrasney@gmail.com>
2019-01-01 15:44:40 +0200

    serve static files

--- CourseAndrewMead/WebServerExpress/server.js
+++ CourseAndrewMead/WebServerExpress/server.js
@@ -4,17 +4,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({hello : "express"});
+  res.send({ hello: "express" });
 });
 
 app.get("/about", (req, res) => {
   res.send("<h1>This is about</h1>");
 });
 
-app.get("*", (req, res) => {
-  res.send("Not found");
-});
-
+// --- mount middleware to handle html file inside public file
+app.use(express.static(__dirname + "/public"));
 
 // --- listen for requests on this port
 app.listen(3000);
