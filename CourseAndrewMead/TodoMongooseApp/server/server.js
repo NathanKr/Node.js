bdc36171c4cba7f7e4b9f99e7ff403af187404f8
Nathan Krasney <natankrasney@gmail.com>
2019-01-21 11:50:33 +0200

    use POST and body-parser

--- CourseAndrewMead/TodoMongooseApp/server/server.js
+++ CourseAndrewMead/TodoMongooseApp/server/server.js
@@ -2,6 +2,8 @@ console.log("app is loading");
 
 const mongoose = require("./db/mongoose");
 const express = require("express");
+
+// --- used to get isolated body under 
 const bodyParser = require("body-parser");
 
 const { User } = require("./db/models/user"); // --- model of user
@@ -10,10 +12,12 @@ const { Note } = require("./db/models/note"); // --- model of note
 const port = 3000;
 
 const app = express();
+
+// used for json inside body ?
 app.use(bodyParser.json());
 
 app.post("/notes", (req, res) => {
-  console.log("recived post", req);
+  console.log("recived post body", req.body);
 });
 
 app.listen(port, () => {
