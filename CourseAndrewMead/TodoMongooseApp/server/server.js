f3a45575e4b225921fe8bfaf6a935077a980eada
Nathan Krasney <natankrasney@gmail.com>
2019-01-21 13:32:10 +0200

    POST using mongoose

--- CourseAndrewMead/TodoMongooseApp/server/server.js
+++ CourseAndrewMead/TodoMongooseApp/server/server.js
@@ -1,9 +1,11 @@
 console.log("app is loading");
 
-const mongoose = require("./db/mongoose");
+
+const mongoose = require("./db/mongoose");// --- this line is a MUST
+
 const express = require("express");
 
-// --- used to get isolated body under 
+// --- used to get isolated body under
 const bodyParser = require("body-parser");
 
 const { User } = require("./db/models/user"); // --- model of user
@@ -17,7 +19,18 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post("/notes", (req, res) => {
-  console.log("recived post body", req.body);
+  const { text, completed } = req.body;
+  const newNote = new Note({ text, completed });
+  newNote
+    .save()
+    // --- send the object back including mongoDB object id 
+    .then(doc => res.send(doc))
+    /*
+     --- in most cases its client problem so send 400 (but what if its DB problem?)
+     --- status and error is sent back to client 
+     --- error can include mongoose validation error
+     */
+    .catch(err => res.status(400).send(err));
 });
 
 app.listen(port, () => {
