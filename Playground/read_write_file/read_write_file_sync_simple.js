fa3c5a865a31590b0caf92d7b422c505fb3ae88a
Nathan Krasney <natankrasney@gmail.com>
2019-07-23 13:03:33 +0300

    simple sync read write

---
+++ Playground/read_write_file/read_write_file_sync_simple.js
@@ -0,0 +1,17 @@
+const os = require("os");
+const fs = require("fs");
+
+
+const data = "Some content!";
+const path = "test.txt";
+const optionsWrite = {flag  : 'a'}; // append , by default create file if does not exist
+const optionsRead = {encoding : 'utf8'};
+
+try {
+  fs.writeFileSync(path, data+os.EOL , optionsWrite);
+  //file written successfully
+  const text = fs.readFileSync(path , optionsRead);
+  console.log(text);
+} catch (err) {
+  console.error(err);
+}
